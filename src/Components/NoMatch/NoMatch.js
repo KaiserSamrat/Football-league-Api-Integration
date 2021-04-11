bb45e37b688aa7dda02c8f0f11bc24a766df2e4c
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-04-11 14:00:47 +0600

    message added

--- src/Components/NoMatch/NoMatch.js
+++ src/Components/NoMatch/NoMatch.js
@@ -4,7 +4,7 @@ const NoMatch = () => {
     return (
         <div className="text-white text-center m-5">
             <h1>Error!!</h1>
-            <h3>No Page Found</h3>
+            <h3>No Page Found, Please Try again</h3>
         </div>
     );
 };
